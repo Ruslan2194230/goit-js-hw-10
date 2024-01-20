d5075741ab6cc5057ba8f3fa5979a8dffb4c4b16
Ruslan Osmanov <mega-magnus@ukr.net>
2024-01-20 19:02:01 +0100

    commit-1

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -1,24 +1,41 @@
 // import '../css/common.css';
 // import pokemonCardTpl from '../templates/pokemon-card.hbs';
-import pokemonCardTpl from '../templates';
+// import pokemonCardTpl from '../templates/';
 
 const refs = {
   cardContainer: document.querySelector('.js-card-container'),
+  searchFrom: document.querySelector('.js-search-form'),
 };
 
-fetchPokemon()
-  .then(renderPokemonCard)
-  .catch(error => {
-    console.log(error);
-  });
+refs.searchFrom.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  fetchPokemon(searchQuery)
+    .then(renderPokemonCard)
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => form.reset());
+}
 
 function fetchPokemon(pokemonId) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(response => {
-    return response.json();
-  });
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
+    response => {
+      return response.json();
+    }
+  );
 }
 
 function renderPokemonCard(pokemon) {
   const markup = pokemonCardTpl(pokemon);
   refs.cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert('Ups chto to poslo ne tak')
 }
